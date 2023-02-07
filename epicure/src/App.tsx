637991435247773ffa518f75c9792bbf146d3b04
ronkopiel <ron.kopiel@gmail.com>
2023-02-07 16:07:42 +0200

    basic homepage component

--- epicure/src/App.tsx
+++ epicure/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/homepage/homepage';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
