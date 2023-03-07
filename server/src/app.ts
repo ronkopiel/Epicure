66aabd3c8392547554b54dd514159d14edfc176d
ronkopiel <ron.kopiel@gmail.com>
2023-03-07 18:08:13 +0200

    more backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import routes from "../src/routes/index";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { connectToDB } from '../connection';
 import dotenv from "dotenv"
 dotenv.config()
 const app = express();
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(routes);
+
 app.get("/", function (req, res) {
    res.send("Hello World");
 });
