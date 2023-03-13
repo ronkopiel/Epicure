cb65e5fddd1f77b991007b0dfc37b8686021ef14
ronkopiel <ron.kopiel@gmail.com>
2023-03-13 02:13:41 +0200

    filters popup

--- epicure/src/components/FilterButton.tsx
+++ epicure/src/components/FilterButton.tsx
@@ -1,16 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import {
-  getNewRestaurants,
-  getOpenRestaurants,
-  reInitializeRestaurants,
-  sortPopularity,
-} from ".././features/restaurantsSlicer";
-import {
-  getNewChefs,
-  sortViewCount,
-  reInitializeChefs,
-} from "../features/chefsSlicer";
+
 interface IFilterButton {
   function: Function;
   text: string;
--- epicure/src/components/PopupFilter.tsx
+++ epicure/src/components/PopupFilter.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import { useDispatch, useSelector } from "react-redux";
 import ReactSlider from "react-slider";
-import {  IDishesState, IRestaurantState } from "../data/interface";
+import { IDishesState, IRestaurantState } from "../data/interface";
 
 import { IRootState } from "../store";
 
@@ -22,106 +22,115 @@ const PopupFilter: React.FC<IFilter> = (props) => {
   const dispatch = useDispatch();
   const [max, setMax] = useState(0);
   const [min, setMin] = useState(0);
-  const filter = props.filter;
   const [fiterdMax, setFilteredMax] = useState(max);
   const [fiterdMin, setFilteredMin] = useState(min);
-  switch (filter) {
+  const [clearButtonState, setClearButtonState] = useState(false);
+  const [filterMenuState, setFilterMenuState] = useState("hide");
+  switch (props.filter) {
     case "price":
       dishes.map((dish) => {
-        if (dish.price > max) {setMax(dish.price);
-          setFilteredMax(dish.price)}
-        else if (dish.price < min) {setMin(dish.price)
-          setFilteredMin(dish.price)
-        };
+        if (dish.price > max) {
+          setMax(dish.price);
+          setFilteredMax(dish.price);
+        } else if (dish.price < min) {
+          setMin(dish.price);
+          setFilteredMin(dish.price);
+        }
       });
       break;
     case "rating":
-      
-      restaurants.map((resaturant) => {        
-        if (resaturant.rating > max) {setMax(resaturant.rating)
-        setFilteredMax(resaturant.rating)}
-        else if (resaturant.rating < min){ setMin(resaturant.rating)
-        setFilteredMin(resaturant.rating)};
+      restaurants.map((resaturant) => {
+        if (resaturant.rating > max) {
+          setMax(resaturant.rating);
+          setFilteredMax(resaturant.rating);
+        } else if (resaturant.rating < min) {
+          setMin(resaturant.rating);
+          setFilteredMin(resaturant.rating);
+        }
       });
       break;
+    default:
+      break;
+  }
+  useEffect(() => {
+    switch (props.filter) {
+      case "price":
+        const filteredDishes = dishes.filter(
+          (dish) => dish.price <= fiterdMax && dish.price >= fiterdMin
+        );
+        dispatch(props.slicer(filteredDishes));
+        break;
+      case "rating":
+        const filteredRestaurants = restaurants.filter(
+          (resaturant) =>
+            resaturant.rating <= fiterdMax && resaturant.rating >= fiterdMin
+        );
+        dispatch(props.slicer(filteredRestaurants));
+        break;
       default:
         break;
-      }
-      useEffect(() => {
-        switch (filter) {
-          case "price":
-            const filteredDishes = dishes.filter(
-              (dish) => dish.price <= fiterdMax && dish.price >= fiterdMin
-            );
-            dispatch(props.slicer(filteredDishes));
-            break;
-            case "rating":
-              const filteredRestaurants = restaurants.filter(
-                (resaturant) => resaturant.rating <= fiterdMax && resaturant.rating >= fiterdMin
-              );
-              dispatch(props.slicer(filteredRestaurants));
-              break;
-          default:
-            break;
-        }
-      }, [fiterdMax, fiterdMin]);
-
+    }
+  }, [fiterdMax, fiterdMin]);
+  const toggleFillterMenu = () =>  filterMenuState === "hide"? setFilterMenuState(""): setFilterMenuState("hide")
+    
+  
   return (
-    <Dropdown className="popup-filter-container">
-      <Dropdown.Toggle
-        variant="success"
-        id="dropdown-basic"
-        className="popup-filter popup-filter-content"
-      >
-        {props.name}
-        <img src="/assets/dropdown.svg" alt="" className="dropdown" />
-      </Dropdown.Toggle>
+    <>
+    <div className="popup-filter">
 
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">
-          <div className="popup">
-            <div className="range-container">
-              {props.name} selected
-              <div className="range">
-                {min}-{max}
-              </div>
-            </div>
-            <div className="slider-container">
-              <div className="curent-min-max">
-                <div className="curent-min">{fiterdMin}</div>
-                <div className="curent-max">{fiterdMax}</div>
-              </div>
-              <ReactSlider
-                className="horizontal-slider"
-                thumbClassName="example-thumb"
-                trackClassName="example-track"
-                defaultValue={[min, max]}
-                min={min}
-                max={max}
-                onChange={(value, index) => {
-                  switch (index) {
-                    case 1:
-                      setFilteredMax(value[index]);
+      <button onClick={toggleFillterMenu} className="popup-filter-content">
+        <span>{props.name}</span>
+        <img src="/assets/dropdown.svg" alt="" className="dropdown-arrow" />
+      </button>
 
-                      break;
-                    case 0:
-                      setFilteredMin(value[index]);
-                      break;
-                    default:
-                      break;
-                  }
-                }}
-                ariaLabel={["Lower thumb", "Upper thumb"]}
-                ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
-                renderThumb={(props, state) => <div {...props}></div>}
-                pearling
-                minDistance={0}
-              />
-            </div>
+      <div className={filterMenuState + " popup"}>
+        <div className="range-container">
+          <span>{props.name} selected</span>
+          <div className="range">
+            <span>
+              {min}-{max}
+            </span>
           </div>
-        </Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
+        </div>
+        <div className="slider-container">
+          <div className="curent-min-max">
+            <div className="curent-min">{fiterdMin}</div>
+            <div className="curent-max">{fiterdMax}</div>
+          </div>
+          <ReactSlider
+            className="horizontal-slider"
+            thumbClassName="example-thumb"
+            trackClassName="example-track"
+            defaultValue={[min, max]}
+            min={min}
+            max={max}
+            onChange={(value, index) => {
+              if (value[index] !== min && value[index] !== max)
+                setClearButtonState(true);
+              else setClearButtonState(false);
+              switch (index) {
+                case 1:
+                  setFilteredMax(value[index]);
+
+                  break;
+                case 0:
+                  setFilteredMin(value[index]);
+                  break;
+                default:
+                  break;
+              }
+            }}
+            ariaLabel={["Lower thumb", "Upper thumb"]}
+            ariaValuetext={(state) => `Thumb value ${state.valueNow}`}
+            renderThumb={(props, state) => <div {...props}></div>}
+            pearling
+            minDistance={0}
+          />
+        </div>
+        <button id="clear-button" className={clearButtonState? "": "hide"} onClick={() => {setFilteredMax(max);setFilteredMin(min)}}>CLEAR</button>
+      </div>
+    </div>
+    </>
   );
 };
 export default PopupFilter;
